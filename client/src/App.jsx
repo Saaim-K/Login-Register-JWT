f1fd63c0d1675c3426422930b648f7d4a682e7be
Saaim-K <saaimkhan221@gmail.com>
2022-12-31 21:30:54 +0500

    Commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import { React, useState } from 'react'
 
 const App = () => {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
   return (
     <>
-      App
+      <form>
+        name<input type="text" onChange={(e) => { setName(e.target.value) }} /> <br />
+        email<input type="text" onChange={(e) => { setEmail(e.target.value) }} /> <br />
+        password<input type="text" onChange={(e) => { setPassword(e.target.value) }} /> <br />
+      </form>
+      {name}
+      {email}
+      {password}
     </>
   )
 }
