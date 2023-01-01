786bcc6db5fecb0528655f55eb8054058ccb72c1
Saaim-K <saaimkhan221@gmail.com>
2023-01-01 22:59:14 +0500

    .

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,57 @@
 import { React, useState } from 'react'
+import axios from 'axios'
+
+let baseUrl = '';
+if (window.location.href.split(':')[0] === 'http') { baseUrl = 'http://localhost:5426' }
 
 const App = () => {
-  const [name, setName] = useState('')
+  const [firstName, setFirstName] = useState('')
+  const [lastName, setLastName] = useState('')
+  const [age, setAge] = useState('')
   const [email, setEmail] = useState('')
+  const [gender, setGender] = useState('')
+  const [phone, setPhone] = useState('')
   const [password, setPassword] = useState('')
+  const [confirmpassword, setConfirmPassword] = useState('')
+
+
+
+
+  const createUser = (e) => {
+    e.preventDefault();
+    axios.post(`${baseUrl}/user`,
+      { firstName, lastName, age, email, gender, phone, password, confirmpassword })
+      .then((response) => {
+        console.log("Response Sent ", response);
+      })
+      .catch(error => {
+        console.log('Error occured', error)
+      })
+  }
+
+
+
   return (
     <>
-      <form>
-        name<input type="text" onChange={(e) => { setName(e.target.value) }} /> <br />
-        email<input type="text" onChange={(e) => { setEmail(e.target.value) }} /> <br />
-        password<input type="text" onChange={(e) => { setPassword(e.target.value) }} /> <br />
+      <form onSubmit={createUser}>
+        firstName <input type="text" name='firstName' onChange={(e) => { setFirstName(e.target.value) }} /> <br />
+        lastName <input type="text" name='lastName' onChange={(e) => { setLastName(e.target.value) }} /> <br />
+        age <input type="number" name='age' onChange={(e) => { setAge(e.target.value) }} /> <br />
+        email <input type="text" name='email' onChange={(e) => { setEmail(e.target.value) }} /> <br />
+        gender <input type="text" name='gender' onChange={(e) => { setGender(e.target.value) }} /> <br />
+        phone <input type="number" name='phone' onChange={(e) => { setPhone(e.target.value) }} /> <br />
+        password <input type="password" name='password' onChange={(e) => { setPassword(e.target.value) }} /> <br />
+        confirmpassword <input type="password" name='confirmpassword' onChange={(e) => { setConfirmPassword(e.target.value) }} /> <br />
+        <button type='submit'>Create User</button>
+        <h1>{firstName}</h1>
+        <h1>{lastName}</h1>
+        <h1>{age}</h1>
+        <h1>{email}</h1>
+        <h1>{gender}</h1>
+        <h1>{phone}</h1>
+        <h1>{password}</h1>
+        <h1>{confirmpassword}</h1>
       </form>
-      {name}
-      {email}
-      {password}
     </>
   )
 }
--- server/server.mjs
+++ server/server.mjs
@@ -1,15 +1,107 @@
 import express from 'express'
+import path from 'path'
 import cors from 'cors'
+import mongoose from 'mongoose'
+
 
 const app = express()
-const port = process.env.PORT || 5555
+const port = process.env.PORT || 5426
+const mongodbURI = process.env.mongodbURI || "mongodb+srv://login-signup:login-signup@cluster0.pu4fwyo.mongodb.net/Login-Singnup?retryWrites=true&w=majority"
+
 
 app.use(cors())
 app.use(express.json());
+mongoose.connect(mongodbURI)
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+// ----------------------------------- MongoDB -----------------------------------
+let userSchema = new mongoose.Schema({
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    age: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    gender: { type: String, required: true },
+    phone: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    confirmpassword: { type: String, required: true },
+    createdOn: { type: Date, default: Date.now }
 })
+const userModel = mongoose.model('Users', userSchema);
+// ----------------------------------- MongoDB -----------------------------------
+
+
+// ----------------------------------- Create User -----------------------------------
+app.post('/user', async (req, res) => {
+    try {
+        const password = req.body.password
+        const confirmpassword = req.body.confirmpassword
+        if (password === confirmpassword) {
+            userModel.create(
+                {
+                    firstName: req.body.firstName,
+                    lastName: req.body.lastName,
+                    age: req.body.age,
+                    email: req.body.email,
+                    gender: req.body.gender,
+                    phone: req.body.phone,
+                    password: req.body.password,
+                    confirmpassword: req.body.confirmpassword,
+                }
+            )
+            res.status(201).send(
+                `user created`
+            )
+            console.log(response.data)
+        } else {
+            res.send("Passwords didn't Match")
+        }
+    } catch (error) {
+        res.status(400).send(error)
+    }
+})
+// ----------------------------------- Create User -----------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+////////////////mongodb connected disconnected events///////////////////////////////////////////////
+mongoose.connection.on('connected', function () {//connected
+    console.log("Mongoose is connected");
+    // process.exit(1);
+});
+
+mongoose.connection.on('disconnected', function () {//disconnected
+    console.log("Mongoose is disconnected");
+    process.exit(1);
+});
+
+mongoose.connection.on('error', function (err) {//any error
+    console.log('Mongoose connection error: ', err);
+    process.exit(1);
+});
+
+process.on('SIGINT', function () {/////this function will run jst before app is closing
+    console.log("app is terminating");
+    mongoose.connection.close(function () {
+        console.log('Mongoose default connection closed');
+        process.exit(0);
+    });
+});
+////////////////mongodb connected disconnected events///////////////////////////////////////////////
+
+
+// const __dirname = path.resolve()
+// const staticPath = path.join(__dirname, '../client/build')
+// console.log("PATH-------",staticPath)
+// app.use('/'.express.static(staticPath))
+// app.use('*'.express.static(staticPath))
 
 
 app.listen(port, () => {
